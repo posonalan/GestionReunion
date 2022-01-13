be1ac407d44addc3f36b469f8928de52b8645792
mok59100 <moktar.terki@gmail.com>
2022-01-13 15:57:53 +0100

    test

--- GestionReunion/PHP/MODEL/MANAGER/TestManager.Class.php
+++ GestionReunion/PHP/MODEL/MANAGER/TestManager.Class.php
@@ -3,10 +3,37 @@
 //*********************************UserManager**************************** *//
 
 //ADD UserManager : ok
-// $m = new Utilisateurs(["nomUtilisateur"=>"test","prenomUtilisateur"=>"Alan","emailUtilisateur"=>"alan.poson@gmail.com","motDePasseUtilisateur"=>"test","validationUtilisateur"=>true,"idRole"=>1]);
+//  $m = new Utilisateurs(["nomUtilisateur"=>"test","prenomUtilisateur"=>"Alan","emailUtilisateur"=>"alan.poson@gmail.com","motDePasseUtilisateur"=>"test","validationUtilisateur"=>true,"idRole"=>1]);
 // var_dump($m);
 // $D = UtilisateursManager::add($m);
 // var_dump($D);
+
+// //Update UserManager : ok
+// $m = UtilisateursManager::findById(18);
+// var_dump($m);
+// $m->setNomUtilisateur("pierrik");
+// var_dump($m);
+// UtilisateursManager::update($m);
+
+// //Delete UserManager : ok
+// $m = UtilisateursManager::findById(20);
+// UtilisateursManager::delete($m);
+
+// //Get Liste UserManager : ok
+// $tableau = UtilisateursManager::getList();
+// foreach ($tableau as $info)
+// {
+//     echo $info->toString();
+// }
+
+
+//*********************************UserManager**************************** *//
+
+// ADD UserManager : ok
+ $m = new Taches(["libelleTache"=>"test","dateEcheanceTache"=>2009-12-20,"idEtatAvancement"=>1,"idUtilisateur"=>1,"idPrioriteTache"=>1]);
+var_dump($m);
+$D = TachesManager::add($m);
+var_dump($D);
 
 // //Update UserManager : ok
 // $m = UtilisateursManager::findById(18);
