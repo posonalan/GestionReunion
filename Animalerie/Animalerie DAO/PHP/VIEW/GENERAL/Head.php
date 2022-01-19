e21ac7fa8decb0b3c68c258fc29b24c5190fa437
mok59100 <moktar.terki@gmail.com>
2022-01-19 11:43:20 +0100

    gere

--- Animalerie/Animalerie DAO/PHP/VIEW/GENERAL/Head.php
+++ Animalerie/Animalerie DAO/PHP/VIEW/GENERAL/Head.php
@@ -6,16 +6,19 @@
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
 <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+        integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 
 
 	<link rel="stylesheet" href="./CSS/style.css">
 	<link rel="stylesheet" href="./CSS/root.css">
     <link rel="stylesheet" href="./CSS/menu.css">
 
-	<link rel="stylesheet" href="../CSS/form.css">
-	<link rel="stylesheet" href="../CSS/index.php">
-	 <link rel="stylesheet" href="../CSS/login.css">
+	<link rel="stylesheet" href="./CSS/form.css">
+	<link rel="stylesheet" href="./CSS/index.php">
+	 <link rel="stylesheet" href="./CSS/login.css">
 	
    
 </head>
